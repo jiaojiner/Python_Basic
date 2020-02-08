3d9850a7239d0098501c3ff51a8caca7c457f6d7
jiaojiner <15148365776@163.com>
2020-02-08 12:40:05 +0800

    commit 1

--- 内置类型_列表-列表引用/ifconfig_ip.py
+++ 内置类型_列表-列表引用/ifconfig_ip.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- enconding -*-
+# -*- enconding = utf-8 -*-
 #该代码由本人学习时编写，仅供自娱自乐！
 #本人QQ：1945962391 
 #欢迎留言讨论，共同学习进步！
@@ -7,8 +7,24 @@
 import os #引入os模块，可支持执行系统命令
 import re #引入正则表达式模块
 
-ifconfig_result = os.popen("ifconfig "+'ens33').read() #z执行Linux命令并将结果赋值
+ifconfig_result = os.popen("ifconfig "+'ens33').read() #执行Linux命令并将结果赋值
 # print(ifconfig_result)
+list_ifconfig_result = ifconfig_result.split('\n') #将命令返回结果以\n为分割形成列表
+# print(list_ifconfig_result)
+for x in list_ifconfig_result:
+    # print(x.split())
+    for y in x.split():
+        if re.match('.*(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}).*',y):
+            for ip in y:
+                if y.split('.')[-1] == '255':
+                    mask = y
+                elif y.split('.')[-1] == '0':
+                    network = y
+                else:
+                    ipv4 = y
+print('%-15s : %-15s' % ('IP地址为',ipv4))
+print('%-15s : %-15s' % ('子网掩码为',mask))
+print('%-15s : %-15s' % ('广播地址为',network))
 
 
 
