b48960db97204f6a829cde3d5b3c4ae19a028591
jiaojiner <15148365776@163.com>
2020-02-11 12:23:03 +0800

    语法与语句练习提交1

---
+++ 语法语句/gateway.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- enconding = utf-8 -*-
+#该代码由本人学习时编写，仅供自娱自乐！
+#本人QQ：1945962391 
+#欢迎留言讨论，共同学习进步！
+
+import os
+
+route_result = os.popen('route -n').read()
+# print(route_result)
+route_result_list = route_result.split('\n')
+# print(route_result_list)
+for x in route_result_list[2:-1]:
+        if 'UG' == x.split()[3]:
+            print('网关地址为：'+ x.split()[1])
