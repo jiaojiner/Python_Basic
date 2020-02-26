c173965033b89ab2058686f048088c3325953791
jiaojiner <15148365776@163.com>
2020-02-26 20:56:31 +0800

    海象运算符

---
+++ 函数及模块/test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- encoding = utf-8 -*-
+# 该代码由本人学习时编写，仅供自娱自乐！
+# 本人QQ：1945962391 
+# 欢迎留言讨论，共同学习进步！
+
+
+# 海象运算符使用方式
+while True:
+    if (n := input('请输入参数：')) == '1':
+        print(n)
+        break
+    else:
+        print('错误')
