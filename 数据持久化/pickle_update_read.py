026bc84c571e51b18d18e5b4be7098d0c491e2ef
jiaojiner <15148365776@163.com>
2020-02-28 12:31:01 +0800

    数据持久化

--- 数据持久化/pickle_update_read.py
+++ 数据持久化/pickle_update_read.py
@@ -4,24 +4,44 @@
 # 本人QQ：1945962391 
 # 欢迎留言讨论，共同学习进步！
 
-from initdata import db
+
 import pickle
 import time
 
-print('测试更改文件内容')
-print('='*20)
-db_file_result = open('pickle_test.pl', 'br')
-db_read_result = pickle.load(db_file_result)
-db_file_result.close()
-db_read_result['cq_bomb']['pay'] *= 1.6
-dbfile_write = open('pickle_test.pl', 'bw')
-pickle.dump(db_read_result, dbfile_write)
-dbfile_write.close()
-print('更改文件程序执行完毕')
-print('='*20)
-time.sleep(2)
-print('即将开始读取文件')
-dbfile_read = open('pickle_test.pl', 'rb')
-read_result = pickle.load(dbfile_read)
+db = {'192.168.98.129': '可达', '192.168.98.128': '可达', '192.168.98.12': '不可达'}
+
+# print('测试更改文件内容')
+# print('='*20)
+# db_file_result = open('pickle_test.pl', 'br')
+# db_read_result = pickle.load(db_file_result)
+# db_file_result.close()
+# db_read_result['192.168.98.12'] = '不可达'
+# dbfile_write = open('pickle_test.pl', 'bw')
+# pickle.dump(db_read_result, dbfile_write)
+# dbfile_write.close()
+# print('更改文件程序执行完毕')
+# print('='*20)
+# time.sleep(2)
+# print('即将开始读取文件')
+# dbfile_read = open('pickle_test.pl', 'rb')
+# read_result = pickle.load(dbfile_read)
+# print(read_result)
+# dbfile_read.close()
+
+print('读取文件内容：')
+dbfile = open('pickle_test.pl', 'ba+')
+dbfile.seek(0, 0)
+read_result = pickle.load(dbfile)
 print(read_result)
-dbfile_read.close()
+time.sleep(2)
+print('更改文件内容!')
+read_result['192.168.98.12'] = '可达'
+# print(dbfile.tell())
+dbfile.seek(0, 0)
+pickle.dump(read_result, dbfile)
+time.sleep(2)
+dbfile.seek(0, 0)
+print('开始读取文件：')
+read_next_result = pickle.load(dbfile)
+print(read_result)
+dbfile.close()
--- 数据持久化/pickle_wirte_read.py
+++ 数据持久化/pickle_wirte_read.py
@@ -4,19 +4,38 @@
 # 本人QQ：1945962391 
 # 欢迎留言讨论，共同学习进步！
 
-from initdata import db
+
 import pickle
 import time
 
-dbfile = open('pickle_test.pl', 'bw')
+db = {'192.168.98.129': '可达', '192.168.98.128': '可达', '192.168.98.12': '不可达'}
+
+# 先将字典内容写进文件后再重新读取文件内容
+# dbfile = open('pickle_test.pl', 'bw')
+# pickle.dump(db, dbfile)
+# dbfile.close()
+# print('写入程序执行完毕')
+# print('='*20)
+# time.sleep(2)
+# print('即将开始读取文件')
+# dbfile_read = open('pickle_test.pl', 'rb')
+# read_result = pickle.load(dbfile_read)
+# print(read_result)
+# dbfile_read.close()
+
+# 直接以追加的方式写入文件，然后移动指针到文件首部进行读取
+print('开始写入文件！')
+dbfile = open('pickle_test.pl', 'ba+')
 pickle.dump(db, dbfile)
-dbfile.close()
-print('写入程序执行完毕')
-print('='*20)
 time.sleep(2)
-print('即将开始读取文件')
-dbfile_read = open('pickle_test.pl', 'rb')
-read_result = pickle.load(dbfile_read)
+print('写入完毕，开始移动指针位置！')
+dbfile.seek(0, 0)
+print('指针位置移动到：', dbfile.tell())
+time.sleep(2)
+print('开始读取文件内容！')
+read_result = pickle.load(dbfile)
 print(read_result)
-dbfile_read.close()
+time.sleep(2)
+print('读取文件完成，关闭文件')
+dbfile.close()
 
