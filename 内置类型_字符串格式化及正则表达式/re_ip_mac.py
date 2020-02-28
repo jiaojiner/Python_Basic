6fbb83cdfed67b61a753275a1654efbabe4c02fc
jiaojiner <15148365776@163.com>
2020-02-28 11:36:56 +0800

    优化实现方式

--- 内置类型_字符串格式化及正则表达式/re_ip_mac.py
+++ 内置类型_字符串格式化及正则表达式/re_ip_mac.py
@@ -9,6 +9,8 @@ import re  # 导入正则表达式模块
 str1 = 'Port-channel1.189          192.168.189.254  YES     CONFIG   up                       up '
 str2 = '166    54a2.74f7.0326    DYNAMIC     Gi1/0/11'
 
+# str1_result = re.search('(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})',str1).groups()
+# str1_result = re.findall('(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})',str1)
 str1_result = re.match('\s*(\w.*\d+)\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+(\w*)\s+(\w*)\s+(\w*)\s+(\w*)\s.*',
                        str1).groups()
 str2_result = re.match('\s*\d+\s*(\w{1,4}\.\w{1,4}\.\w{1,4})\.*', str2).groups()
